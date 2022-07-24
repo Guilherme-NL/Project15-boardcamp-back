bef9235042d2d68f9c3959f85f1993f5a6c5e6d1
Guilherme-NL <guilherme.nunes.lopes@gmail.com>
2022-07-23 23:29:56 -0300

    feat: add get customers controller

--- src/controllers/customersController.js
+++ src/controllers/customersController.js
@@ -1,7 +1,24 @@
 import { connection } from "../dbStrategy/postgres.js";
+import joi from "joi";
 
 export async function getCustomers(req, res) {
-  res.sendStatus(500);
+  const cpf = parseInt(req.params.cpf);
+  try {
+    if (cpf) {
+      const { rows: cpfCustomers } = await connection.query(
+        "SELECT * FROM customers WHERE cpf LIKE $1",
+        [cpf + "%"]
+      );
+      res.send(cpfCustomers);
+    } else {
+      const { rows: customers } = await connection.query(
+        "SELECT * FROM customers"
+      );
+      res.send(customers);
+    }
+  } catch {
+    res.sendStatus(404);
+  }
 }
 
 export async function postCustomers(req, res) {
